249c5ebeb9373383ef2f3de01a30a9d9ce7cb733
Wicky <codewicky@163.com>
2016-11-19 04:26:30 +0800

    Initial Commit

---
+++ DWLyricLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DWLyricLabel
+//
+//  Created by Wicky on 16/11/19.
+//  Copyright © 2016年 Wicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DWLyricLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DWLyricLabel
+//
+//  Created by Wicky on 16/11/19.
+//  Copyright © 2016年 Wicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
